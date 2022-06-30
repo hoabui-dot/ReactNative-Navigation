134497e2a21007b174dce15ae78840049de06347
Hoa Bui <58062470+hoabui-dot@users.noreply.github.com>
2022-06-30 11:53:15 +0700

    login form has worked

--- App.js
+++ App.js
@@ -4,40 +4,16 @@ import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from './src/components/LoginForm/LoginForm';
+import Todos from './src/components/Todos/Todos';
 
 const Stack = createNativeStackNavigator();
-
-function DetailsScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-
-function HomeScreen({navigation}) {
-  return (
-    <View>
-      <Text style={{color: 'red', fontSize: 40}}>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Login')}
-      />
-    </View>
-  );
-}
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: ''}}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Todos} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,14 +1,76 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text, Image, View, SafeAreaView} from 'react-native';
+import {
+  TextInput,
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  Pressable,
+} from 'react-native';
 import styles from './LoginFormStyle';
 import logo from '../../images/logo.png';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function LoginForm() {
+export default function LoginForm({navigation}) {
+  const [account, setAccount] = React.useState({username: '', password: ''});
+
+  React.useEffect(() => {
+    getTodosFromUserDevice();
+    console.log(account);
+  }, []);
+
+  React.useEffect(() => {
+    saveTodoToUserDevice(account);
+  }, [account]);
+
+  const saveTodoToUserDevice = async account => {
+    try {
+      const stringifyTodos = JSON.stringify(account);
+      await AsyncStorage.setItem('account', stringifyTodos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTodosFromUserDevice = async () => {
+    try {
+      const account = await AsyncStorage.getItem('account');
+      if (account != null) {
+        setAccount(JSON.parse(account));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView styles={styles.container}>
       <View styles={styles.content}>
         <Image style={styles.titleLogo} source={logo} />
+        <TextInput
+          style={styles.name}
+          placeholder="UserName"
+          onChangeText={e => setAccount({...account, username: e})}
+        />
+
+        <TextInput
+          style={styles.password}
+          placeholder="Password"
+          onChangeText={e => setAccount({...account, password: e})}
+          secureTextEntry
+        />
+        <Pressable
+          style={styles.loginButton}
+          onPress={() => {
+            navigation.navigate('Home');
+          }}>
+          <Text style={styles.loginText}>Login</Text>
+        </Pressable>
+        <View style={styles.showAccount}>
+          <Text>User Name: {account.username}</Text>
+          <Text>Password: {account.password}</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
